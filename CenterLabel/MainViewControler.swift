cf2f1faa95301c5bb3dcde2cc98844798d65ec14
Danila Bolshakov <danila1999777@gmail.com>
2023-04-29 22:18:28 +0300

    Initial commit

---
+++ CenterLabel/MainViewControler.swift
@@ -0,0 +1,47 @@
+//
+//  MainViewControler.swift
+//  CenterLabel
+//
+//  Created by IOS - Developer  on 22.04.2023.
+//
+
+import UIKit
+
+struct Element {
+    let symbol: String
+    let name: String
+}
+
+class MainViewController: UIViewController {
+    
+    let elementList = ["Harley-Davison", "Mercedes", "Opel", "Porsche"]
+    var currentElementIndex = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+        func updateElement() {
+            answerLabel.text = "?"
+            let nameElement = elementList[currentElementIndex]
+            let image = UIImage(named: nameElement)
+            imageView.image = image
+        }
+    
+    @IBAction func showAnswerButtonDidPressed(_ sender: Any) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    @IBAction func nextElementDidPressed(_ sender: UIButton) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
+    
+}
+
